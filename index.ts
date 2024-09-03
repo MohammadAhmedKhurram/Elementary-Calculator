ba6449c869492f4b8a6b91ddd57af38393c13395
MohammadAhmedKhurram <162457856+MohammadAhmedKhurram@users.noreply.github.com>
2024-09-03 15:43:03 +0500

    --

--- index.ts
+++ index.ts
@@ -3,8 +3,16 @@
 import inquirer from "inquirer";
 
 const answer = await inquirer.prompt([
-  { message: "Enter first number", type: "number", name: "firstNumber" },
-  { message: "Enter second number", type: "number", name: "secondNumber" },
+  { 
+    message: "Enter first number",
+    type: "number",
+    name: "firstNumber" 
+  },
+  {
+    message: "Enter second number",
+    type: "number",
+    name: "secondNumber" 
+  },
   {
     message: "Select one of the operator to perform operation",
     type: "list",
@@ -14,14 +22,11 @@ const answer = await inquirer.prompt([
 ]);
 
 if (answer.operator == "Addition") {
-    console.log(answer.firstNumber + answer.secondNumber);
-}
-else if (answer.operator == "Substraction") {
-console.log(answer.firstNumber - answer.secondNumber);
-}
-else if (answer.operator == "Multiplication") {
+  console.log(answer.firstNumber + answer.secondNumber);
+} else if (answer.operator == "Substraction") {
+  console.log(answer.firstNumber - answer.secondNumber);
+} else if (answer.operator == "Multiplication") {
   console.log(answer.firstNumber * answer.secondNumber);
-}
-else if (answer.operator == "Division") {
+} else if (answer.operator == "Division") {
   console.log(answer.firstNumber / answer.secondNumber);
 }
